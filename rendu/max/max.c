637e1c6dc28d7eb68605636746d3e398049c2abf
AliBastida <teclados.raspar0n@icloud.com>
2023-01-17 19:34:30 +0100

    max acabado. Tendria que comprobar si los negativos son necesarios

--- rendu/max/max.c
+++ rendu/max/max.c
@@ -6,11 +6,12 @@
 /*   By: abastida <abastida@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:15:27 by abastida          #+#    #+#             */
-/*   Updated: 2023/01/17 15:21:55 by abastida         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:33:27 by abastida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int	max(int *tab, unsigned int len)
 {
@@ -19,19 +20,23 @@ int	max(int *tab, unsigned int len)
 
 	i = 0;
 	tmp = tab[0];
-	while(tab[i])
+	while(i < (int)len)
 	{
 		if(tmp < tab[i])
+		{
 			tmp = tab[i];
+		}
 		i++;
 	}
-
-	if(len == 0)
-		return(tmp);
+	return(tmp);
 }
 
 int	main(void)
 {
-
-
+	int temp;
+	int	numbers[12] = {1, 4, 2, 5, 8, 15, 300, 22, 1432, 3, 8333333, 34};
+	
+	temp = max(numbers, 12);
+	printf("%d\n", temp);
+	return(0);
 }
