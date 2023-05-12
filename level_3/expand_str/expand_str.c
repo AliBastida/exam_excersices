232ef369b1a5e5b4c23115cd80c51c419f8f6e9e
Alicia Bastida sanchez <abastida@cbr10s4.42barcelona.com>
2023-05-12 14:20:54 +0200

    vamos

--- level_3/expand_str/expand_str.c
+++ level_3/expand_str/expand_str.c
@@ -3,50 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   expand_str.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abastida <abastida@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: abastida <abastida@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 17:05:05 by abastida          #+#    #+#             */
-/*   Updated: 2023/05/11 18:54:46 by abastida         ###   ########.fr       */
+/*   Created: 2023/05/12 13:30:03 by abastida          #+#    #+#             */
+/*   Updated: 2023/05/12 13:30:03 by abastida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	expand_str(char *s1)
+void expand_str(char *s1)
 {
 	int i = 0;
-	while (s1[i] && s1[i] == ' ')
-		i++;
-	while (s1[i])
-	{
-		if (s1[i] != ' ')
-		{
-			if(s1[i - 1] == ' ' && i)
-				write(1, "   ", 3);
-			write(1, &s1[i], 1);
-		}
-		i++;
-	}
-
-}
-
-/*void expand_str(char *s1)
-{
-	int i = 0;
-	while(s1[i] && (s1[i] == ' ' || s1[i] == '	'))
-		i++;
+	while(*s1 && *s1 == ' ')
+		s1++;
 	while(s1[i])
 	{
-		if(((s1[i] == ' ') ||(s1[i] == '	')) && ((s1[i+1] != ' ' && s1[i+1] != '	')))
-			write(1, "   ", 3);
-		if (s1[i] != ' ' || s1[i] != '	')
+		if(s1[i] != ' ')
 			write(1, &s1[i], 1);
+		else if (s1[i] == ' ' && s1[i+1] == '\0' )
+			break ;
+		else if(s1[i] == ' ' && (s1[i + 1] != ' '))
+			write (1, "   ", 3);
 		i++;
 	}
 }
-*/
-int	main(int ac, char **av)
+
+
+int main(int ac, char **av)
 {
 	if (ac == 2)
 		expand_str(av[1]);
